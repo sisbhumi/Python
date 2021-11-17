547ac6fb590888cfc30229ae28f43a65ec2c2b51
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2021-11-17 14:24:27 +0530

    String

---
+++ Que_41.py
@@ -0,0 +1,5 @@
+#41)Write a Python program to count occurrences of a substring in a string.
+str1 = 'Chandu k chacha ne chandu ki chachi ko chandi ki chamch se chutny chaty'
+print()
+print(str1.count("cha"))
+#ans 7
